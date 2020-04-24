f14ae7e2bc42420ca2b730d1fe20c54e8d5942c4
Mayank Khurmai <mayankkhurmai8@gmail.com>
2020-04-24 22:12:50 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 
+function onbodyload(){
+    setTimeout(afterloader, 300);
+
+}
+
+function afterloader(){
+    document.getElementById("my-loader").style.display = "none";
+    document.getElementById("full-body").style.display = "block";
+}
